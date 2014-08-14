e1e104071edfcd7b1b14b4ab6e32a8a8ec8952dd
arti24n <st.stas@mail.ru>
2014-08-14 18:22:06 +0400

    nicroposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140802065152) do
+ActiveRecord::Schema.define(version: 20140814101252) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
